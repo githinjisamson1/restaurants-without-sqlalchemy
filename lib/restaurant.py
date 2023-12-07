1969e886a3e88eb6a3ac45a1562a4ed28e3fdad9
githinjisamson1 <githinjisamson148@gmail.com>
2023-12-07 07:52:26 +0300

    Complete restaurant ORM

--- lib/restaurant.py
+++ lib/restaurant.py
@@ -1,11 +1,25 @@
 
+from review import Review
 
 class Restaurant:
+    # Restaurant constructor
     def __init__(self, name):
         self._name = name
-        
+    
+    # getter
     def name(self):
         return self._name
     
+    def reviews(self):
+        return [item for item in Review.allReviews]
+    
+    def customers(self):
+        return [item for item in Review.allCustomers]
+    
+# instantiation
 r1 = Restaurant("Figo")
+
+# test working
 print(r1.name())
+print(r1.reviews())
+print(r1.customers())
