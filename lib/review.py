e179a7ded15ae48f3e43ca76dfef156a55d420a9
githinjisamson1 <githinjisamson148@gmail.com>
2023-12-07 07:51:35 +0300

    Complete review ORM

--- lib/review.py
+++ lib/review.py
@@ -1,14 +1,18 @@
 
 class Review:
+    # keep track of instances
     allReviews = []
+    allCustomers = []
     
+    # Review constructor
     def __init__(self, customer, restaurant, rating):
-        self.customer = customer
-        self.restaurant = restaurant
+        self._customer = customer
+        self._restaurant = restaurant
         self.rating = rating if type(rating) in (int, float) else "Rating must be a number"
         
-            
+     
         Review.addToAllReviews(self)
+        Review.addToAllCustomers(customer)
         
     def rating(self):
         return self.rating
@@ -20,11 +24,31 @@ class Review:
     @classmethod
     def all(cls):
         return cls.allReviews
-            
-            
+    
+    def customer(self):
+        return self._customer
+    
+    def restaurant(self):
+        return self._restaurant
+    
+    @classmethod
+    def addToAllCustomers(cls, customer):
+        # avoid duplicates
+        if customer not in cls.allCustomers:
+            cls.allCustomers.append(customer)
+        
+
+# instatiation 
 r1 = Review("John", "Figo", 4)
+r3 = Review("John", "Figo", 4)
+r4 = Review("Juliana", "Figo", 4)
 # r2 = Review("Peter", "Abc", "k")
-print([item.rating for item in Review.allReviews])
+
+# test workig
+# print([item.rating for item in Review.allReviews])
 print(Review.all())
 print(Review.allReviews)
 print(r1.rating)
+print(r1.customer())
+r1.customer = "Jane"
+print(r1.restaurant())
